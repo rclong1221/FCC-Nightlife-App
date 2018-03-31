7b0f548cbac8e235403274ab8001d8a6833d99a2
R.C. Long <deac.long@wsu.edu>
2018-03-31 00:13:47 -0700

    Boilerplate + index view

---
+++ src/models/users.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+var User = new Schema({
+	twitter: {
+		id: String,
+		displayName: String,
+		username: String,
+	},
+	latestSearch: String
+})
+
+module.exports = {
+	User: mongoose.model('User', User),
+	UserSchema: User
+}
